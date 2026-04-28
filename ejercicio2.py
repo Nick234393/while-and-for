b1a38e089a601fabe26291d6c720f76cdaeac01a
Nick234393 <quiroznicolas345@gmail.com>
2026-04-28 10:02:27 -0400

    Add files via upload

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,8 +1,10 @@
+#piramide
+piramide_numerica = ""
 numero_positivo = int(input("Ingrese numero entero positivo: "))
 while numero_positivo < 0:
     print("Error ingrese otro numero")
     numero_positivo = int(input("Ingrese numero entero positivo: "))
 
 for numero_positivo in range(1,numero_positivo,2):
-    print("#" * numero_positivo)
-
+    piramide_numerica = str(numero_positivo) + "" +piramide_numerica
+    print(piramide_numerica)
---
+++ for.py
@@ -0,0 +1,5 @@
+#ejemplos
+for cualquiercosa in range(0,20,+1,):
+    print("Tiempo restante es: ", cualquiercosa)
+    if cualquiercosa == 7:
+        print("Llego la hora")
